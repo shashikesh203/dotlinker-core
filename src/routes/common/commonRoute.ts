b5abb20bb76d6b7feec8075372fd54c7a928fcd6
shashikesh234 <uitshashikesh@gmail.com>
2026-02-21 10:11:44 +0530

    feat: protected route middleware added in common route

--- src/routes/common/commonRoute.ts
+++ src/routes/common/commonRoute.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import commonController from "../../controller/common/commonController";
 import authorization from "../../middleware/authValidator";
+import { isDoctor, isPatient } from "../../middleware/roleValidator";
 
 
 const commonRoute = express.Router();
 
-commonRoute.get("/get-doctor-details",authorization, commonController.getDoctorProfile);  
-commonRoute.get("/get-patient-details",authorization, commonController.getPatientProfile);  
+commonRoute.get("/get-doctor-details",authorization, isDoctor, commonController.getDoctorProfile);  
+commonRoute.get("/get-patient-details",authorization, isPatient, commonController.getPatientProfile);  
 export default commonRoute;
 
 
